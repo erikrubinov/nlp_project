82df2626f40d5e533f5253221f1ca1f569aa83a1
Niclas Stoffregen <stoffregen@mailbox.org>
2024-07-29 20:41:44 +0200

    smaller changes
    (cherry picked from commit 762e32e855227fff1bf3d2f6305dfe75a3d1d55e)

--- ass2/preprocessing_task_2.py
+++ ass2/preprocessing_task_2.py
@@ -2,28 +2,28 @@ import re
 from helpers import load_datasets
 
 
-def preprocess_data(data_en, data_fr):
+def preprocess_data(source_data, target_data):
     # Lowercase the text
     """
     Normalizing case helps reduce the complexity of the language model by treating words like “The” and “the” as the same word, which can be particularly helpful in languages like English where capitalization is more stylistic than semantic.
     """
-    data_en['text'] = data_en['text'].str.lower()
-    data_fr['text'] = data_fr['text'].str.lower()
+    source_data['text'] = source_data['text'].str.lower()
+    target_data['text'] = target_data['text'].str.lower()
 
     # Remove XML tags
     """
     Lines containing XML-tags are likely not actual conversational or formal text but rather formatting or metadata which is irrelevant for translation purposes
     """
-    data_en['text'] = data_en['text'].apply(lambda x: '' if x.strip().startswith('<') else x)
-    data_fr['text'] = data_fr['text'].apply(lambda x: '' if x.strip().startswith('<') else x)
+    source_data['text'] = source_data['text'].apply(lambda x: '' if x.strip().startswith('<') else x)
+    target_data['text'] = target_data['text'].apply(lambda x: '' if x.strip().startswith('<') else x)
 
     # Strip empty lines and remove their correspondences
     """
      Empty lines or lines that do not contain any meaningful content should be removed because they do not provide valuable information for training the model. It is also important to remove the corresponding line in the other language to maintain alignment.
     """
-    mask = (data_en['text'].str.strip().astype(bool) & data_fr['text'].str.strip().astype(bool))
-    data_en = data_en[mask]
-    data_fr = data_fr[mask]
+    mask = (source_data['text'].str.strip().astype(bool) & target_data['text'].str.strip().astype(bool))
+    source_data = source_data[mask]
+    target_data = target_data[mask]
 
     # Define the characters to remove
     noisy_characters = re.escape('@#$%^&*~<>|\\{}[]+=_/')
@@ -32,16 +32,15 @@ def preprocess_data(data_en, data_fr):
     regex_pattern = f'[{noisy_characters}]'
 
     # Remove noisy characters using regex substitution
-    data_en['text'] = data_en['text'].apply(lambda x: re.sub(regex_pattern, '', x))
-    data_fr['text'] = data_fr['text'].apply(lambda x: re.sub(regex_pattern, '', x))
+    source_data['text'] = source_data['text'].apply(lambda x: re.sub(regex_pattern, '', x))
+    target_data['text'] = target_data['text'].apply(lambda x: re.sub(regex_pattern, '', x))
+
+    return source_data, target_data
 
 
-    return data_en, data_fr
+def prepare_data(fraction, source_file_path="fr-en/europarl-v7.fr-en.en",
+                 target_file_path="fr-en/europarl-v7.fr-en.fr"):
+    source_data, target_data = load_datasets(fraction, source_file_path, target_file_path)
+    preprocessed_source, preprocessed_target = preprocess_data(source_data, target_data)
 
-
-
-def prepare_data(fraction):
-    english_data, french_data = load_datasets(fraction)
-    preprocessed_en, preprocessed_fr = preprocess_data(english_data, french_data)
-
-    return preprocessed_en, preprocessed_fr
+    return preprocessed_source, preprocessed_target
